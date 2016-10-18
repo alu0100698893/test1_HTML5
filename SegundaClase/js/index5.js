4d83761b3625c8960df2baa8d22fd676b85dee4f
Alberto Martinez <alu0100698893@ull.es>
2016-10-18 19:37:14 +0100

    Toastr

---
+++ SegundaClase/js/index5.js
@@ -0,0 +1,14 @@
+// Función de ventana de alerta
+function mensaje() {
+
+  // error, success, info, warning
+  toastr.error('Aquí el mensaje de texto.', 'Título de notificación');
+
+  toastr.options = {
+    closeButton: true,
+    progressBar: true,
+    positionClass: 'toast-top-right',
+    preventDuplicates: false,
+    timeOut: 2000,
+  }
+}
