d7b318801b3786ce6f39beb59e1b1065cdb9e03a
Alberto Martinez <alu0100698893@ull.es>
2016-10-18 19:23:18 +0100

    SweetAlert

---
+++ SegundaClase/js/index4.js
@@ -0,0 +1,11 @@
+function mensaje() {
+
+  swal({
+    title: 'Título',
+    text: 'Mensaje de texto',
+    html: '<p>Mensaje de texto con <strong>formato</strong>.</p>',
+    type: 'error',
+    timer: 3000,
+  });
+
+}
