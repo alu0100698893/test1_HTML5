4f6f389da8812f387b1bab169933af8950c3ffa4
Alberto Martinez <alu0100698893@ull.es>
2016-10-18 19:08:51 +0100

    Mejorando js oiubounce

--- SegundaClase/js/index3.js
+++ SegundaClase/js/index3.js
@@ -1,6 +1,8 @@
-ouibounce(document.getElementById('modal-card'), {
-  aggressive: true,            // borra la cookie viewedOuibounceModal
-  timer: 5                        // espera (evita falsos positivos)
+$(document).ready(function() {
+  ouibounce(document.getElementById('modal-card'), {
+    aggressive: true,            // borra la cookie viewedOuibounceModal
+    timer: 5                        // espera (evita falsos positivos)
+  });
 });
 
 // Opcional (activa a los 5 segundos)
