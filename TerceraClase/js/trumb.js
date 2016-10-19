13f11e057caed7d504b4395ab48819bba5fb5cf6
Alberto Martinez <alu01006988963@ull.es>
2016-10-19 18:41:28 +0100

    trumbowyg

---
+++ TerceraClase/js/trumb.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.editor').trumbowyg({
+    //svgPath: '/icons/icons.svg', // path of icons.svg
+    // Habitualmente, se indica la ruta del SVG en este parámetro, pero
+    // en codepen, simplemente pegaremos el contenido del SVG en la parte
+    // del código HTML
+  });
+  $('#trumbowyg').trumbowyg('html'); // Obtienes el HTML generado
+});
