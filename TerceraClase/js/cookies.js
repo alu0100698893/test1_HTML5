0654c4d21972a4b985b500a273fadc31214f9ec9
Alberto Martinez <alu01006988963@ull.es>
2016-10-19 16:34:10 +0100

    Segunda prueba con cookies

--- TerceraClase/js/cookies.js
+++ TerceraClase/js/cookies.js
@@ -1,4 +1,12 @@
 // Opción 1: Inicializar cookieBar (genérico, en inglés)
-$(document).ready(function() {
+/*$(document).ready(function() {
   $.cookieBar();
+});
+*/
+
+// Opción 2: Personalizar mensaje (indicar clase)
+$(document).ready(function() {
+  $('.cookie-message').cookieBar({
+    closeButton: '.close'
+  });
 });
