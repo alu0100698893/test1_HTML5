b975de32856032821ccbacb96ab24677cc8d2939
Alberto Martinez <alu01006988963@ull.es>
2016-10-19 17:24:01 +0100

    Awesomplete con HTML y JS opciones nuevas

--- TerceraClase/js/awesomplete.js
+++ TerceraClase/js/awesomplete.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
   var input = document.getElementById('datos');
   new Awesomplete(input, {
-    list: ["Ada", "Java", "JavaScript", "Brainfuck", "LOLCODE", "Node.js", "Ruby on Rails"]
+    list: ["Ada", "Java", "JavaScript", "Brainfuck", "LOLCODE", "Node.js", "Ruby on Rails"],
+    minChars: 1,
+    maxChars: 4,
+    autoFirst: true
   });
 });
