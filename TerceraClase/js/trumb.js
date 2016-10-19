05558315b2b6966b6db2875690a3ee26b32438e0
Alberto Martinez <alu01006988963@ull.es>
2016-10-19 18:51:31 +0100

    trumbowyg arreglos js

--- TerceraClase/js/trumb.js
+++ TerceraClase/js/trumb.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
   $('.editor').trumbowyg({
+    lang: 'es', // Idioma (es.min.js incluido)
+    btns: [
+          ['bold', 'italic', 'underline', 'strikethrough'],
+          ['superscript', 'subscript'],
+          ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+          ['unorderedList', 'orderedList'],
+          ['link', 'insertImage'],
+          ['horizontalRule', 'removeformat'],
+          ['viewHTML'],
+          ['fullscreen']
+        ],
 
   });
   $('#trumbowyg').trumbowyg('html'); // Obtienes el HTML generado
