c9d01698540c4776192d3cc172cce14eaa24aea6
Alberto Martinez <alu0100698893@ull.es>
2016-10-20 16:33:12 +0100

    chartist cambiando tipo de grafica

--- CuartaClase/js/chartist.js
+++ CuartaClase/js/chartist.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
   var data = {
-    labels: ['CYA', 'LPP', 'IA', 'AEDA', 'GPI', 'SOA'],
-    series: [{
-              data: [20, 10, 8, 9, 1, 15]
-            }]
+    labels: ['Oscar', 'Moi', 'Alex', 'Ale', 'Noel', 'Alber'],
+    series: [
+             {data: [20, 10, 8, 9, 1, 15]},
+             {data: [5, 10, 12, 6, 2, 0]},
+             {data: [4, 6, 17, 4, 3, 2]}] //ct_series-a...z clase por defecto que se asigna a la linea
   };
 
   var options = {}
 
-  new Chartist.Line('.chart', data, options);
+  new Chartist.Bar('.chart', data, options);
 });
