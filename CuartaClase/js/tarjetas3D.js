68b375d3e5936d30d75e75fe13c2cf9fe7c36b4e
Alberto Martinez <alu0100698893@ull.es>
2016-10-20 18:46:16 +0100

    tarjetas3D library

--- CuartaClase/js/tarjetas3D.js
+++ CuartaClase/js/tarjetas3D.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   // Configuramos la librería jQuery Flip
   $('#card').flip({
-    axis: 'y',                // 'x' para giro en eje X
+    axis: 'x',                // 'x' para giro en eje X
     trigger: 'click',        // 'hover': giro sobre tarjeta
     speed: 500,                // Velocidad del giro
   });
