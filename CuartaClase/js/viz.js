5743399e4e2eb1cd85ee24bdab4189665658412a
Alberto Martinez <alu0100698893@ull.es>
2016-10-20 17:23:26 +0100

    Viz.js

--- CuartaClase/js/viz.js
+++ CuartaClase/js/viz.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var sample = 'digraph g { a -> b; a -> c; b -> b ; c -> a; c-> b; }';
+  var sample = 'digraph g { a -> b [label= "1"]; a -> c [label= "1"]; b -> b [label= "0"] ; c -> a [label= "0"]; c-> b [label= "1"]; }';
 
   var options = {
     format: 'svg'
