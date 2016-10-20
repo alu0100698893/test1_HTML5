a2a596b91ce32d57d37921e18070cab8ca5fee10
Alberto Martinez <alu0100698893@ull.es>
2016-10-20 16:35:13 +0100

    chartist cambiando tipo de grafica 2

--- CuartaClase/js/chartist.js
+++ CuartaClase/js/chartist.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
   var data = {
-    labels: ['Oscar', 'Moi', 'Alex', 'Ale', 'Noel', 'Alber'],
+    //labels: ['Oscar', 'Moi', 'Alex', 'Ale', 'Noel', 'Alber'],
     series: [
-             {data: [20, 10, 8, 9, 1, 15]},
-             {data: [5, 10, 12, 6, 2, 0]},
-             {data: [4, 6, 17, 4, 3, 2]}] //ct_series-a...z clase por defecto que se asigna a la linea
+             {data: [20, 10, 8, 9, 1, 15]}
+        //     {data: [5, 10, 12, 6, 2, 0]},
+        //     {data: [4, 6, 17, 4, 3, 2]}] //ct_series-a...z clase por defecto que se asigna a la linea
   };
 
-  var options = {}
+  var options = {
+    // donut: true,
+    // donutWidth: 40,
+    // startAngle: 270,
+    // total: 200
+  }
 
-  new Chartist.Bar('.chart', data, options);
+  new Chartist.Pie('.chart', data, options);
 });
