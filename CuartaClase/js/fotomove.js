f3f16abdd0e39eb81a22ad8e508545e7e25ab21a
Alberto Martinez <alu0100698893@ull.es>
2016-10-20 19:33:08 +0100

    jsPDF library

---
+++ CuartaClase/js/fotomove.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  var PSV = PhotoSphereViewer({
+  panorama:  'http://www.hotel-r.net/im/hotel/au/skyline-8.jpg',
+  container: 'photosphere',
+  loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
+  });
+
+});
