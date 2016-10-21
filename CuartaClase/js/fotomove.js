72202ff055c8272aaed897f93f1b8106ccc797d0
Alberto <alu0100698893@ull.edu.es>
2016-10-21 16:33:32 +0100

    libreria para presentaciones 3D

--- CuartaClase/js/fotomove.js
+++ CuartaClase/js/fotomove.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   var PSV = PhotoSphereViewer({
-  panorama:  'http://www.juanmare.com/juanmare/uploads/5/4/8/1/5481026/1302722203.jpg',
+  panorama:  '',
   container: 'photosphere',
   loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
   });
